9acdea7eb43cd17a236a388729d9074fb2e882f2
Speykious <speykious.the.king@gmail.com>
2020-04-06 03:05:00 +0200

    Recompiling

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 export declare const tuple: <T extends any[]>(...data: T) => T;
 export { ParserState, ErrorMsgProvider } from './ParserState';
-export { Parser, MatchTransformer, ParserStateTransformer } from './Parser';
+export { Parser, MatchTransformer, ParserStateTransformer, ParserTuple } from './Parser';
 export { between, choice, join, many, manyJoin, sequenceOf } from './ParserCombinators';
 export { digits, uint, newlines, reg, spaces, str, word, letters } from './ParserCreators';
