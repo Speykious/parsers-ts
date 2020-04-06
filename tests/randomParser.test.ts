25b9017d6e99c523f1ff1d27eba76ec4ce711529
Speykious <speykious.the.king@gmail.com>
2020-04-06 17:22:06 +0200

    Test using a randomDigits parser

--- tests/randomParser.test.ts
+++ tests/randomParser.test.ts
@@ -1,4 +1,4 @@
-import { Parser, tuple } from "../src";
+import { Parser, tuple, ParserState } from "../src";
 
 const pad = (
 	n: number | string,
@@ -14,13 +14,13 @@ const pad = (
 const getRandits = (length: number) =>
 	pad(Math.floor(Math.random() * Math.pow(10, length)), length);
 
-const randomDigits = (length: number = 6) => {
+const randomDigits = (length: number) => {
 	if (length <= 0)
 		throw new Error(`The length has to be greater than 0.`);
 	
 	const randits = getRandits(length);
 
-	return new Parser(inputState => {
+	return new Parser<[string, string]>(inputState => {
 		const issl = inputState.targetString.length;
 		if (issl === 0)
 			return inputState.errorify(`... You won't even attempt to guess?`);
@@ -35,7 +35,58 @@ const randomDigits = (length: number = 6) => {
 			return inputState.errorify(`... You got the length just right, but your guess is wrong!`);
 		
 		else
-			return inputState.update(issl, tuple('Congratulations You finally got it right!', randits))
+			return inputState.update(issl, tuple('Congratulations, you finally got it right!', randits))
 	})
 }
 
+test('Random digits: null length', () => {
+	let error: string = null;
+
+	try {
+		randomDigits(0).run('something');
+	} catch (err) {
+		error = err;
+	}
+
+	expect(/greater than 0/.test(error)).toBe(true);
+})
+
+test('Random digits: no guess', () => {
+	const regnoguess = /attempt to guess/;
+	for (let i = 2; i < 10; i++)
+		expect(regnoguess.test(
+			randomDigits(i).run('').error
+		)).toBe(true);
+})
+
+test('Random digits: guess too short', () => {
+	const regtooshort = /too short/;
+	for (let i = 2; i < 10; i++)
+		expect(regtooshort.test(
+			randomDigits(i).run(getRandits(i-1)).error
+		)).toBe(true);
+})
+
+test('Random digits: guess too long', () => {
+	const regtoolong = /too long/;
+	for (let i = 2; i < 10; i++)
+		expect(regtoolong.test(
+			randomDigits(i).run(getRandits(i+1)).error
+		)).toBe(true);
+})
+
+test('Random digits: guess until correct', () => {
+	const toGuess = randomDigits(2);
+	const wrong = /but your guess is wrong/;
+	let state: ParserState<[string, string]>;
+
+	for (let i = 0; i < 100; i++) {
+		state = toGuess.run(pad(i, 2))
+		if (!state.result)
+			expect(wrong.test(state.error)).toBe(true);
+		else break;
+	}
+
+	console.log(state);
+	expect(/finally got it right/.test(state.result[0])).toBe(true);
+})
