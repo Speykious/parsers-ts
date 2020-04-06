0aed10ac73a71bedfb94645beff532b8bdbef53f
Speykious <speykious.the.king@gmail.com>
2020-04-06 02:54:21 +0200

    Testing...

--- tests/typepbs.test.ts
+++ tests/typepbs.test.ts
@@ -1,11 +1,12 @@
-import { sequenceOf, uint, word, tuple } from "../src"
+import { sequenceOf, uint, letters, tuple } from "../src"
 
 test('Testing types with sequenceOf', () => {
-	const someParsers = tuple(uint, word, uint);
+	const someParsers = tuple(uint, letters, uint);
 	const someSequence = sequenceOf(someParsers);
 	const state1 = someSequence.run('123yaaay321');
 
 	console.log(state1);
-	// expect(state1.error).toBe(null);
-	// expect(state1.result).toEqual([123, 'yaaay', 321]);
+	
+	expect(state1.error).toBe(null);
+	expect(state1.result).toEqual([123, 'yaaay', 321]);
 })
