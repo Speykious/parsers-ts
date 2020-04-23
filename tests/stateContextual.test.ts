195b4fc66201beba7529609fca9ba80c9dcb936e
Speykious <speykious.the.king@gmail.com>
2020-04-24 01:58:48 +0200

    stateContextual test, yeet

--- tests/stateContextual.test.ts
+++ tests/stateContextual.test.ts
@@ -4,14 +4,14 @@ import { ParserState } from '../src'
 
 const greatParser = stateContextual<string, string | string[]>(function* () {
 	const first: ParserState<string> = yield str('Hello')
-	if (first.error) {
-		return first.resultify('First nope.')
-	}
+	if (first.error) return first.resultify('First nope.')
+	
 	const second: ParserState<string> = yield spaces
-	if (second.error) {
-		return second.resultify('Second nope.')
-	}
+	if (second.error) return second.resultify('Second nope.')
+	
 	const third: ParserState<string> = yield str('World!')
+	if (third.error) return second.resultify('Third nope.')
+	
 	return third.resultify([first.result, second.result, third.result])
 })
 
@@ -21,6 +21,12 @@ test('The Great State-Contextual Parser', () => {
 	const state3 = greatParser.run('Hello   ')
 	const state4 = greatParser.run('Hello World')
 	const state5 = greatParser.run('Hello World!')
+
+	expect(state1.result).toBe('First nope.')
+	expect(state1.result).toBe('Second nope.')
+	expect(state1.result).toBe('Third nope.')
+	expect(state1.result).toBe('Third nope.')
+	expect(state1.result).toEqual(['Hello', ' ', 'World!'])
 	
 	console.log(state1, state2, state3, state4, state5)
 })
